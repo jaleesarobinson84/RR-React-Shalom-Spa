30c564cd2b6cbb97d6b9b18b2f9a316d100c9c89
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-03-07 19:48:26 -0500

    initial commit

---
+++ shalom/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+
+
+
+return (
+    <div className="App">
+      <Router>
+        <header>
+          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+
+          <div className="navBar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/packages">Our Packages</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+
+        <div className="display">
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+        </div>
+
+      </Router>
+
+    </div>
+  );
+
+export default App;
+
+
+
+
+
